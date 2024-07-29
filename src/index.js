acb5976255b0d679ef31a9738bd5e3a2b309b253
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-07-29 12:47:27 -0400

    Finishing up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const submit = document.querySelector("#submit");
+const messagesArray = [];
+
+class createNewMessage{
+    constructor(name, email, phone, message){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.message = message;
+    }
+}
+
+submit.addEventListener('click', () => {
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const phone = document.querySelector("#phone");
+    const message = document.querySelector("#message"); 
+    let newMessage = new createNewMessage(name, email, phone, message);
+    messagesArray.push(newMessage);
+    document.querySelector('form').reset();
+})
